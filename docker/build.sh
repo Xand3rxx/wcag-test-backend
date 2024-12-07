dc32fd38782ec15664c451117b7c596df6ee1ca1
Anthony Joboy <35226606+Xand3rxx@users.noreply.github.com>
2024-12-07 15:14:07 +0100

    feat: Added Dockerfile and docker-compose.yml to containerize the application.

---
+++ docker/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# update application cache
+composer install
+cp .env.development .env
+php artisan key:generate --force
+php artisan optimize:clear
+chmod -R 777 /var/www/bootstrap
+chmod -R 777 /var/www/storage
+
+# start the application
+php-fpm -D
